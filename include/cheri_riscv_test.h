186ddfed67124ace13e789bb291f328d67b7d157
Ben Laurie <ben@links.org>
2023-07-03 16:41:14 +0100

    Check causes.

--- include/cheri_riscv_test.h
+++ include/cheri_riscv_test.h
@@ -1,5 +1,5 @@
 #define EXTRA_INIT                                                     \
-  cspecialrw ct0, DDC, c0;                                              \
-  cspecialrw c0, MTDC, ct0;
+  cspecialr ct0, DDC;                                                  \
+  cspecialw MTDC, ct0;
 
 #include <riscv_test.h>
